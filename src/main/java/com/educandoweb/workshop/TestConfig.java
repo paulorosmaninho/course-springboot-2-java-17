9cb06b315d0963f75802e190eb9a710313ea28c5
Paulo Rosmaninho <paulo.rosmaninho@gmail.com>
2021-12-08 16:27:48 -0300

    OrderStatus enum

--- src/main/java/com/educandoweb/workshop/TestConfig.java
+++ src/main/java/com/educandoweb/workshop/TestConfig.java
@@ -12,6 +12,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.educandoweb.workshop.entities.Order;
 import com.educandoweb.workshop.entities.User;
+import com.educandoweb.workshop.entities.enums.OrderStatus;
 import com.educandoweb.workshop.repositories.OrderRepository;
 import com.educandoweb.workshop.repositories.UserRepository;
 
@@ -41,11 +42,11 @@ public class TestConfig implements CommandLineRunner{
 		
 		//A data/hora abaixo está no formato UTC e pode ser calculada
 		//para o valor local de quem desejar.
-		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1); 
-		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2); 
-		Order o3 = new Order(null, Instant.parse("2019-07-22T13:42:22Z"), u1); 
-		Order o4 = new Order(null, Instant.parse("2019-08-20T17:51:29Z"), u3); 
-		Order o5 = new Order(null, Instant.parse("2019-08-19T12:32:25Z"), u4); 
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1); 
+		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT,u2); 
+		Order o3 = new Order(null, Instant.parse("2019-07-22T13:42:22Z"), OrderStatus.DELIVERED,u1); 
+		Order o4 = new Order(null, Instant.parse("2019-08-20T17:51:29Z"), OrderStatus.SHIPPED,u3); 
+		Order o5 = new Order(null, Instant.parse("2019-08-19T12:32:25Z"), OrderStatus.CANCELED,u4); 
 
 		userRepository.saveAll(Arrays.asList(u1, u2, u3, u4));
 		
